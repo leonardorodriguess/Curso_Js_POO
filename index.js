2c57d1db415cc6eb33b18ae72edda3a1e846ece1
Leonardo Rodrigues <leo-feitosa@live.com>
2022-08-21 16:17:05 -0300

    Get e set, contrutor e variável estatica

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 
-const conta1 = new ContaCorrente();
-
-conta1.cliente = new Cliente();
-conta1.cliente.nome = "Ricardo";
-conta1.cliente.cpf = 5456465465;
+const conta1 = new ContaCorrente(new Cliente("Ricardo", 5456465465), 1001);
 
 conta1.depositar(100);
 conta1.sacar(50);
-conta1.agencia = 1001;
 
 
+const cliente2 = new Cliente("Julia", 546546545);
+const conta2 = new ContaCorrente(cliente2, 1002);
 
-const cliente2 = new Cliente();
-const conta2 = new ContaCorrente();
-
-cliente2.nome = "Julia";
-cliente2.cpf = 546546545;
-
-conta2.cliente = cliente2;
 conta2.depositar(5000);
 conta2.sacar(100);
-conta2.agencia = 1002;
-
 
 conta2.transferir(200, conta1);
 
-//console.log(cliente1)
 console.log(conta1)
 console.log(conta2)
+console.log(ContaCorrente.numeroDeContas)
